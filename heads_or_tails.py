26e2cb11e5ed8b4152aaee4a4ef00c048b930c7c
Aditha Ranawaka <adithar.24243@obhs.school.nz>
2026-08-15 19:12:59 +1200

    Testing random num
    Checking for two possibilities - head or tails (1,0)

---
+++ heads_or_tails.py
@@ -0,0 +1,22 @@
+'''
+    author: Aditha Mansilu Ranawaka
+    date: 10/8/2026
+    version: 
+    desciption: Heads or Tails game
+'''
+#---------------------libraries-------------------
+import random 
+
+#---------------------functions-------------------
+def heads_tails():
+    choice= random.randint(0,1)
+    return choice 
+#---------------------main routine----------------
+
+if __name__ == "__main__":
+    choice = heads_tails()
+    print(f'this is what i am recieving {choice}')
+
+
+
+
