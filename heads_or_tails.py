660ebad9fbe6d80d935c20a55bb7d29c067abacf
Aditha Ranawaka <adithar.24243@obhs.school.nz>
2026-08-16 11:04:21 +1200

    while loop
    runs the game until someone reaches 2 points. Done to determine an overall winner

--- heads_or_tails.py
+++ heads_or_tails.py
@@ -14,29 +14,31 @@ def heads_tails():
     user_score = 0
     computer_score = 0
 
-    choice= random.randint(0,1)
     options = ['H','T'] 
 
-    computer_guess = options[choice]
-    user_guess = input('Enter H or T: ')
-    
-    print(f'this is what you guessed --> {user_guess}')
-    print(f'this is what the comp guessed --> {computer_guess} {options}')
+    while user_score!=2 and computer_score!=2:
+        choice= random.randint(0,1)
+        computer_guess = options[choice]
+        user_guess = input('Enter H or T: ')
 
-    while(True): 
-        if(user_guess in options):
-            break
-        else:
-            user_guess =str(input('Invalid input. Enter H or T in capital: '))
-    
-    if user_guess == computer_guess:
-        print("It was {}, you guessed {}, you won that round".format(computer_guess,user_guess))
-        user_score +=1
-    else:
-        print("It was {}, you guessed {}, you lost that round".format(computer_guess,user_guess))
-        computer_score +=1
-    print(f'computer score is {computer_score}, and your score is {user_score}')
+        while(True): 
+            if(user_guess in options):
+                break
+            else:
+                user_guess =str(input('Invalid input. Enter H or T in capital: '))
         
+        if user_guess == computer_guess:
+            print("It was {}, you guessed {}, you won that round".format(computer_guess,user_guess))
+            user_score +=1
+        else:
+            print("It was {}, you guessed {}, you lost that round".format(computer_guess,user_guess))
+            computer_score +=1
+
+        # if user_score==2:
+        #     print('You won that game')
+        # elif computer_score==2:
+        #     print('You lost that game')
+            
     # return 
 
     
