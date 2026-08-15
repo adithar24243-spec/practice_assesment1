835b2108265b0695f76dff0ea5852687fbd83dbe
Aditha Ranawaka <adithar.24243@obhs.school.nz>
2026-08-15 20:31:13 +1200

    Score strucutre
    introduce the user input and introduce the score system for the computer and the user.

--- heads_or_tails.py
+++ heads_or_tails.py
@@ -9,9 +9,32 @@ import random
 
 #---------------------functions-------------------
 def heads_tails():
+
+    # Ensure that the player and the computer starts at 0
+    user_score = 0
+    computer_score = 0
+
     choice= random.randint(0,1)
-    options = ['H','T']
-    return options[choice] 
+    options = ['H','T'] 
+
+    computer_guess = options[choice]
+    user_guess = input('Enter H or T: ')
+
+    # if user_guess == 'H' or user_guess == 'T':
+    #     # ensure only valid guesses pesses through
+    #     print(f'You entered {user_guess}, {computer_guess}')
+    
+    if user_guess == computer_guess:
+        print("It was {}, you guessed {}, you won that round".format(computer_guess,user_guess))
+        user_score +=1
+    else:
+        print("It was {}, you guessed {}, you lost that round".format(computer_guess,user_guess))
+        computer_score +=1
+    print(f'computer score is {computer_score}, and your score is {user_score}')
+        
+    # return 
+
+    
 #---------------------main routine----------------
 
 if __name__ == "__main__":
