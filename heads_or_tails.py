2de2236c7c55634d6c2b197c08255e02c083f25d
Aditha Ranawaka <adithar.24243@obhs.school.nz>
2026-08-15 19:28:54 +1200

    Heads or tails
    replacing 0 and 1 with H and T (heads and tails)

--- heads_or_tails.py
+++ heads_or_tails.py
@@ -10,11 +10,13 @@ import random
 #---------------------functions-------------------
 def heads_tails():
     choice= random.randint(0,1)
-    return choice 
+    options = ['H','T']
+    return options[choice] 
 #---------------------main routine----------------
 
 if __name__ == "__main__":
     choice = heads_tails()
+    
     print(f'this is what i am recieving {choice}')
 
 
